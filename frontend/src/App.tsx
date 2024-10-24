e8caf03a69962a21288f6de9eebc8b42d7e30b41
Reda El Marzouki <relmarzouki@gmail.com>
2024-10-24 12:27:25 +0100

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>React App</h1>
+      <p>Open the console to see the output of the server.</p>
+    </>
+  );
+}
+
+export default App;
